6281bebcda123e1f7688b89e0502f8465218ab9d
Tayyab Ali <mtayyabali@hotmail.com>
2021-02-05 06:56:08 +0100

    removed unnecessary checks on params, added comments, removed default example files

--- app/Http/Controllers/BitcoinPriceController.php
+++ app/Http/Controllers/BitcoinPriceController.php
@@ -7,28 +7,44 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
+/**
+ * Main Class for getting BPI
+ * Class BitcoinPriceController
+ * @package App\Http\Controllers
+ */
 class BitcoinPriceController extends BaseController
 {
+    /**
+     * Controller for getting BPI
+     * @param Request $request
+     * @return array|string
+     */
     public function getBPI(Request $request)
     {
         try {
+
+            // Validation of params
             $validator = Validator::make($request->all(), [
                 'startDate' => 'required|date|date_format:Y-m-d',
                 'endDate' => 'required|date|after:startDate|date_format:Y-m-d|before_or_equal:now'
             ]);
+
+            // Check if validation fail
             if ($validator->fails()) {
                 return ['success' => false, 'message' => $validator->errors()];
             }
+
+            // Set Filters
             $filters = [];
-            if ($request->has('startDate')) {
-                $filters['startDate'] = $request->input('startDate');
-            }
-            if ($request->has('endDate')) {
-                $filters['endDate'] = $request->input('endDate');
-            }
+            $filters['startDate'] = $request->input('startDate');
+            $filters['endDate'] = $request->input('endDate');
+
+            // Get Data from Model
             $response = BitcoinPrice::getBitcoinData($filters);
+
+            // Return response
             if ($response) {
-                return ['success' => true, 'data' => (array) $response->bpi];
+                return ['success' => true, 'data' => (array)$response->bpi];
             }
             return ['success' => false, 'message' => 'Data fetch error'];
         } catch (\Exception $exception) {
--- app/Models/BitcoinPrice.php
+++ app/Models/BitcoinPrice.php
@@ -5,11 +5,21 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Http;
 
-
+/**
+ * Model for Bitcoin Price
+ * Class BitcoinPrice
+ * @package App\Models
+ */
 class BitcoinPrice extends Model
 {
+    /**
+     * Function to get BPI data from API
+     * @param array $filters
+     * @return false|mixed|string
+     */
     public static function getBitcoinData($filters = [])
     {
+        // Fetch url from environment variable
         $apiUrl = env('API_URL');
         try {
             if (!empty($filters)) {
